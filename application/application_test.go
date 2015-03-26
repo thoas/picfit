c7fb21806f652f1007e47dca41267eaa32a2a21f
Florent Messa <florent.messa@gmail.com>
2015-03-26 15:49:25 +0100

    Use directly handler in unittests

--- application/application_test.go
+++ application/application_test.go
@@ -1,14 +1,11 @@
 package application
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/thoas/imaging"
-	"github.com/thoas/muxer"
 	"github.com/thoas/picfit/dummy"
 	"github.com/thoas/picfit/engines"
-	"github.com/thoas/picfit/hash"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -24,9 +21,8 @@ type Dimension struct {
 }
 
 type TestRequest struct {
-	URL                string
-	ExceptedDimensions *Dimension
-	Operation          *engines.Operation
+	URL        string
+	Dimensions *Dimension
 }
 
 func newDummyApplication() *Application {
@@ -65,65 +61,45 @@ func TestDummyApplication(t *testing.T) {
 
 	app := newDummyApplication()
 
-	con := app.KVStore.Connection()
-	defer con.Close()
-
 	u, _ := url.Parse(ts.URL + "/avatar.png")
 
 	tests := []*TestRequest{
 		&TestRequest{
 			URL: fmt.Sprintf("http://example.com/display?url=%s&w=50&h=50&op=resize", u.String()),
-			ExceptedDimensions: &Dimension{
+			Dimensions: &Dimension{
 				Width:  50,
 				Height: 50,
 			},
-			Operation: engines.Resize,
 		},
 		&TestRequest{
 			URL: fmt.Sprintf("http://example.com/display?url=%s&w=100&h=0&op=resize", u.String()),
-			ExceptedDimensions: &Dimension{
+			Dimensions: &Dimension{
 				Width:  100,
 				Height: 100,
 			},
-			Operation: engines.Resize,
 		},
 		&TestRequest{
 			URL: fmt.Sprintf("http://example.com/display?url=%s&w=50&h=50&op=thumbnail", u.String()),
-			ExceptedDimensions: &Dimension{
+			Dimensions: &Dimension{
 				Width:  50,
 				Height: 50,
 			},
-			Operation: engines.Thumbnail,
 		},
 	}
 
 	for _, test := range tests {
 		request, _ := http.NewRequest("GET", test.URL, nil)
 
-		req := muxer.NewRequest(request)
+		res := httptest.NewRecorder()
 
-		serialized := hash.Serialize(req.QueryString)
+		handler := app.ServeHTTP(ImageHandler)
 
-		key := hash.Tokey(serialized)
+		handler.ServeHTTP(res, request)
 
-		file, err := app.ImageFileFromRequest(&Request{
-			req,
-			test.Operation,
-			con,
-			key,
-			u,
-			"",
-		}, true, true)
+		img, err := imaging.Decode(res.Body)
 
 		assert.Nil(t, err)
-		assert.NotNil(t, file.Processed)
-
-		result := file.Content()
-
-		img, err := imaging.Decode(bytes.NewReader(result))
-
-		assert.Nil(t, err)
-		assert.Equal(t, img.Bounds().Max.X, test.ExceptedDimensions.Width)
-		assert.Equal(t, img.Bounds().Max.Y, test.ExceptedDimensions.Height)
+		assert.Equal(t, img.Bounds().Max.X, test.Dimensions.Width)
+		assert.Equal(t, img.Bounds().Max.Y, test.Dimensions.Height)
 	}
 }
