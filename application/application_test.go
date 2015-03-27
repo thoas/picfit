9189690021c6252f1680327f1491a2e94e461afd
Florent Messa <florent.messa@gmail.com>
2015-03-27 10:39:43 +0100

    Add assertions for sharding

--- application/application_test.go
+++ application/application_test.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strings"
 	"testing"
 	"time"
 )
@@ -129,6 +130,12 @@ func TestStorageApplication(t *testing.T) {
 	assert.True(t, connection.Exists(key))
 
 	filepath, _ := gokvstores.String(connection.Get(key))
+
+	parts := strings.Split(filepath, "/")
+
+	assert.Equal(t, len(parts), 3)
+	assert.Equal(t, len(parts[0]), 1)
+	assert.Equal(t, len(parts[1]), 1)
 
 	assert.True(t, app.SourceStorage.Exists(filepath))
 }
