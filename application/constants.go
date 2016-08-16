83d605fac229850eac3c470dfa8815afbb5a2fd2
Florent Messa <florent.messa@gmail.com>
2016-08-16 13:19:08 +0200

    Fix version constant

--- application/constants.go
+++ application/constants.go
@@ -1,7 +1,7 @@
 package application
 
 // Version is the current version of picfit
-const Version = "0.4"
+const Version = "0.4.0"
 
 // DefaultContentType is the default content type
 const DefaultContentType = "image/png"
