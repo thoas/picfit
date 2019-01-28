5e9893a7df6606024ccffda6200518dfa597009a
Florent Messa <florent.messa@gmail.com>
2019-01-28 10:10:02 +0100

    feat: allow mimetype handling using backends

--- engine/config/config.go
+++ engine/config/config.go
@@ -1,14 +1,23 @@
 package config
 
+type BackendsConfig struct {
+	Lilliput *BackendConfig `mapstructure:"lilliput"`
+	GoImage  *BackendConfig `mapstructure:"goimage"`
+}
+
+type BackendConfig struct {
+	Mimetypes []string
+}
+
 // Config is the engine config
 type Config struct {
-	Backends        []string `mapstructure:"backends"`
-	DefaultFormat   string   `mapstructure:"default_format"`
-	Format          string   `mapstructure:"format"`
-	Quality         int      `mapstructure:"quality"`
-	MaxBufferSize   int      `mapstructure:"max_buffer_size"`
-	ImageBufferSize int      `mapstructure:"image_buffer_size"`
-	JpegQuality     int      `mapstructure:"jpeg_quality"`
-	PngCompression  int      `mapstructure:"png_compression"`
-	WebpQuality     int      `mapstructure:"webp_quality"`
+	Backends        *BackendsConfig `mapstructure:"backends"`
+	DefaultFormat   string          `mapstructure:"default_format"`
+	Format          string          `mapstructure:"format"`
+	Quality         int             `mapstructure:"quality"`
+	MaxBufferSize   int             `mapstructure:"max_buffer_size"`
+	ImageBufferSize int             `mapstructure:"image_buffer_size"`
+	JpegQuality     int             `mapstructure:"jpeg_quality"`
+	PngCompression  int             `mapstructure:"png_compression"`
+	WebpQuality     int             `mapstructure:"webp_quality"`
 }
