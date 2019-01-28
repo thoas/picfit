a53de860b2f6d04413dab3105630b349f3104431
Florent Messa <florent.messa@gmail.com>
2019-01-28 10:20:20 +0100

    chore: review (thks @gillesfabio)

--- engine/config/config.go
+++ engine/config/config.go
@@ -1,23 +1,23 @@
 package config
 
-type BackendsConfig struct {
-	Lilliput *BackendConfig `mapstructure:"lilliput"`
-	GoImage  *BackendConfig `mapstructure:"goimage"`
+type Backends struct {
+	Lilliput *Backend `mapstructure:"lilliput"`
+	GoImage  *Backend `mapstructure:"goimage"`
 }
 
-type BackendConfig struct {
+type Backend struct {
 	Mimetypes []string
 }
 
 // Config is the engine config
 type Config struct {
-	Backends        *BackendsConfig `mapstructure:"backends"`
-	DefaultFormat   string          `mapstructure:"default_format"`
-	Format          string          `mapstructure:"format"`
-	Quality         int             `mapstructure:"quality"`
-	MaxBufferSize   int             `mapstructure:"max_buffer_size"`
-	ImageBufferSize int             `mapstructure:"image_buffer_size"`
-	JpegQuality     int             `mapstructure:"jpeg_quality"`
-	PngCompression  int             `mapstructure:"png_compression"`
-	WebpQuality     int             `mapstructure:"webp_quality"`
+	Backends        *Backends `mapstructure:"backends"`
+	DefaultFormat   string    `mapstructure:"default_format"`
+	Format          string    `mapstructure:"format"`
+	Quality         int       `mapstructure:"quality"`
+	MaxBufferSize   int       `mapstructure:"max_buffer_size"`
+	ImageBufferSize int       `mapstructure:"image_buffer_size"`
+	JpegQuality     int       `mapstructure:"jpeg_quality"`
+	PngCompression  int       `mapstructure:"png_compression"`
+	WebpQuality     int       `mapstructure:"webp_quality"`
 }
