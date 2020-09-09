46ac956a1e3cd8787f8f6253ede51fa602300858
Yann Salaün <yann.salaun@ulule.com>
2020-09-09 17:46:31 +0200

    fix: fix Resize and Thumbnail methods

--- engine/backend/gifsicle.go
+++ engine/backend/gifsicle.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"image/gif"
 	"os/exec"
 
 	"github.com/thoas/picfit/image"
@@ -32,19 +33,10 @@ func (b *Gifsicle) Flip(*image.ImageFile, *Options) ([]byte, error) {
 }
 
 // Resize implements Backend.
-func (b *Gifsicle) Resize(*image.ImageFile, *Options) ([]byte, error) {
-	return nil, MethodNotImplementedError
-
-}
-
-// Rotate implements Backend.
-func (b *Gifsicle) Rotate(*image.ImageFile, *Options) ([]byte, error) {
-	return nil, MethodNotImplementedError
-}
-
-// Thumbnail implements Backend.
-func (b *Gifsicle) Thumbnail(imgfile *image.ImageFile, opts *Options) ([]byte, error) {
-	cmd := exec.Command("gifsicle", "--resize", fmt.Sprintf("%dx%d", opts.Width, opts.Height))
+func (b *Gifsicle) Resize(imgfile *image.ImageFile, opts *Options) ([]byte, error) {
+	cmd := exec.Command("gifsicle",
+		"--resize", fmt.Sprintf("%dx%d", opts.Width, opts.Height),
+	)
 	cmd.Stdin = bytes.NewReader(imgfile.Source)
 	stdout := new(bytes.Buffer)
 	cmd.Stdout = stdout
@@ -58,4 +50,61 @@ func (b *Gifsicle) Thumbnail(imgfile *image.ImageFile, opts *Options) ([]byte, e
 		return nil, err
 	}
 	return stdout.Bytes(), nil
+}
+
+// Rotate implements Backend.
+func (b *Gifsicle) Rotate(*image.ImageFile, *Options) ([]byte, error) {
+	return nil, MethodNotImplementedError
+}
+
+// Thumbnail implements Backend.
+func (b *Gifsicle) Thumbnail(imgfile *image.ImageFile, opts *Options) ([]byte, error) {
+	img, err := gif.Decode(bytes.NewReader(imgfile.Source))
+	if err != nil {
+		return nil, err
+	}
+	bounds := img.Bounds()
+	left, top, cropw, croph := computecrop(bounds.Dx(), bounds.Dy(), opts.Width, opts.Height)
+
+	cmd := exec.Command("gifsicle",
+		"--crop", fmt.Sprintf("%d,%d+%dx%d", left, top, cropw, croph),
+		"--resize", fmt.Sprintf("%dx%d", opts.Width, opts.Height),
+	)
+	cmd.Stdin = bytes.NewReader(imgfile.Source)
+	stdout := new(bytes.Buffer)
+	cmd.Stdout = stdout
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
+
+	var target *exec.ExitError
+	if err := cmd.Run(); errors.As(err, &target) && target.Exited() {
+		return nil, errors.New(stderr.String())
+	} else if err != nil {
+		return nil, err
+	}
+	return stdout.Bytes(), nil
+}
+
+func computecrop(srcw, srch, destw, desth int) (left, top, cropw, croph int) {
+	srcratio := float64(srcw) / float64(srch)
+	destratio := float64(destw) / float64(desth)
+
+	if srcratio > destratio {
+		cropw = int((destratio * float64(srch)) + 0.5)
+		croph = srch
+	} else {
+		croph = int((float64(srcw) / destratio) + 0.5)
+		cropw = srcw
+	}
+
+	left = int(float64(srcw-cropw) * 0.5)
+	if left < 0 {
+		left = 0
+	}
+
+	top = int(float64(srch-croph) * 0.5)
+	if top < 0 {
+		top = 0
+	}
+	return
 }
